78ac75957baad72705480cd7cd20d9c97cec2540
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2021-02-21 17:36:04 +0800

    Update c27200601.lua

--- script/c27200601.lua
+++ script/c27200601.lua
@@ -13,14 +13,6 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)	
 	local e2=Ritual.CreateProc({handler=c,desc=aux.Stringid(id,1),lvtype=RITPROC_GREATER,filter=aux.FilterBoolFunction(Card.IsCode,27200600),location=LOCATION_DECK})
 	c:RegisterEffect(e2)		
-	-- local e1=Effect.CreateEffect(c)
-	-- e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	-- e1:SetType(EFFECT_TYPE_ACTIVATE)
-	-- e1:SetCode(EVENT_FREE_CHAIN)
-	-- e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
-	-- e1:SetTarget(s.target)
-	-- e1:SetOperation(s.operation)
-	-- c:RegisterEffect(e1)
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e3:SetDescription(aux.Stringid(id,2))
@@ -34,90 +26,6 @@ function s.initial_effect(c)
 end
 s.fit_monster={27200600}
 s.listed_names={27200600}
--- function s.filter(c,e,tp,m,ft)
-	-- if not c:IsRace(RACE_MACHINE) or bit.band(c:GetType(),0x81)~=0x81
-		-- or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
-	-- local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
-	-- if c.mat_filter then
-		-- mg=mg:Filter(c.mat_filter,nil)
-	-- end
-	-- if ft>0 then
-		-- return mg:CheckWithSumGreater(Card.GetRitualLevel,c:GetLevel(),c)
-	-- else
-		-- return mg:IsExists(s.mfilterf,1,nil,tp,mg,c)
-	-- end
--- end
--- function s.apoqlifilter(c,e,tp,m,ft)
-	-- if not c:IsCode(27200600) or bit.band(c:GetType(),0x81)~=0x81
-		-- or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
-	-- local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
-	-- if c.mat_filter then
-		-- mg=mg:Filter(c.mat_filter,nil)
-	-- end
-	-- if ft>0 then
-		-- return mg:CheckWithSumGreater(Card.GetRitualLevel,c:GetLevel(),c)
-	-- else
-		-- return mg:IsExists(s.mfilterf,1,nil,tp,mg,c)
-	-- end
--- end
-
--- function s.mfilterf(c,tp,mg,rc)
-	-- if c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5 then
-		-- Duel.SetSelectedCard(c)
-		-- return mg:CheckWithSumGreater(Card.GetRitualLevel,rc:GetLevel(),rc)
-	-- else return false end
--- end
--- function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	-- local loc=0
-	-- if chk==0 then
-		-- local mg=Duel.GetRitualMaterial(tp)
-		-- if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>1 then
-		-- end
-		-- local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		-- local res=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg,ft)
-		-- if res then return true end
-		-- loc=loc+LOCATION_HAND
-		-- res=Duel.IsExistingMatchingCard(s.apoqlifilter,tp,LOCATION_DECK,0,1,nil,e,tp,mg,ft)
-		-- if res then loc=loc+LOCATION_EXTRA+LOCATION_PZONE end
-		-- return ft>-1 and res
-	-- end
-	-- Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,loc)
--- end
--- function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	-- local mg=Duel.GetRitualMaterial(tp)
-	-- if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>1 then
-	-- end
-	-- local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	-- Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	-- local grp=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,nil,e,tp,mg,ft)
-	-- local grp2=Duel.GetMatchingGroup(s.apoqlifilter,tp,LOCATION_DECK,0,nil,e,tp,mg,ft)
-	-- grp:Merge(grp2)
-	-- local tg=grp:Select(tp,1,1,nil)
-	-- local tc=tg:GetFirst()
-	-- if tc then
-		-- mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
-		-- if tc.mat_filter then
-			-- mg=mg:Filter(tc.mat_filter,nil)
-		-- end
-		-- local mat=nil
-		-- if ft>0 then
-			-- Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			-- mat=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
-		-- else
-			-- Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			-- mat=mg:FilterSelect(tp,s.mfilterf,1,1,nil,tp,mg,tc)
-			-- Duel.SetSelectedCard(mat)
-			-- Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-			-- local mat2=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
-			-- mat:Merge(mat2)
-		-- end
-		-- tc:SetMaterial(mat)
-		-- Duel.ReleaseRitualMaterial(mat)
-		-- Duel.BreakEffect()
-		-- Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
-		-- tc:CompleteProcedure()
-	-- end
--- end
 function s.tokenfilter(c)
 	return c:GetSequence()<5
 end
