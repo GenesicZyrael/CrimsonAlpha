aeebaa36d4539456a1731f12c6ddfe48d6da7412
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2021-06-09 15:58:23 +0800

    Add files via upload

---
+++ script/c34900484.lua
@@ -0,0 +1,15 @@
+--Red-Eyes Zombie Necro Dragon/Assault Mode
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.FALSE)
+	c:RegisterEffect(e1)
+	
+end
+s.listed_names={CARD_ASSAULT_MODE,28240337}
+s.assault_mode=28240337
