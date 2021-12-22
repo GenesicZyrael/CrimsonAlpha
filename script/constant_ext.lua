90b8f98ee143535b1ef2e8bf233ce8193781ab4b
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2021-12-23 00:02:26 +0800

    Add files via upload

---
+++ script/constant_ext.lua
@@ -0,0 +1,14 @@
+--Commonly used hardcoded effects
+EFFECT_ELEMENTAL_CONFLUX    = 55481029
+EFFECT_RITUAL_FROM_DECK		= 27200101
+--Flags for Card.RegisterEffect
+REGISTER_FLAG_SHADDOLL    = 10 
+--Flags for the various filters in the fusion procedure
+
+--Material types constant
+-- MATERIAL_FUSION    = 0x1<<32
+-- MATERIAL_SYNCHRO   = 0x2<<32
+-- MATERIAL_XYZ       = 0x4<<32
+-- MATERIAL_LINK      = 0x8<<32
+--Victory Reasons
+Duel.LoadScript("card_counter_constants_ext.lua")
---
+++ script/crimson_alpha.lua
@@ -0,0 +1,4 @@
+Duel.LoadScript ("constant_ext.lua")
+Duel.LoadScript ("utility_ext.lua")
+Duel.LoadScript ("cards_specific_functions_ext.lua")
+Duel.LoadScript ("proc_ritual_ext.lua")
