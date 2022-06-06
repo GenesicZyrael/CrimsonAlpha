50355736e97664fe187649a5f85226512aa1a159
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-06-07 00:24:01 +0800

    Add files via upload

---
+++ script/custom/c27100502.lua
@@ -0,0 +1,53 @@
+ --Wynn, Charmer of Gusto
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(id,1))
+		e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+		e1:SetProperty(EFFECT_FLAG_DELAY)
+		e1:SetCode(EVENT_TO_GRAVE)
+		e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+		-- e1:SetCondition(s.cond)
+		e1:SetTarget(s.target)
+		e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={27980138}
+s.listed_series={0x10}
+function s.cond(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_EFFECT+REASON_BATTLE)~=0
+end
+function s.filter(c,e,tp)
+	return (c:IsCode(27980138) or c:IsSetCard(0x10)) 
+		and c:IsType(TYPE_SPELL+TYPE_TRAP)
+		and (c:IsSSetable() and c:IsAbleToHand())
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local sft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	if e:GetHandler():IsLocation(LOCATION_HAND) then sft=sft-1 end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local b1,b2,op
+	local sft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	if not Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) then return end
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.SSet(tp,tc)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		if tc:GetFirst():IsType(TYPE_QUICKPLAY) then
+			e1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
+		elseif  tc:GetFirst():IsType(TYPE_TRAP) then
+			e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+		end
+		e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:GetFirst():RegisterEffect(e1)
+	else
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
---
+++ script/custom/c4161732.lua
@@ -0,0 +1,93 @@
+--Amorphageum, the True King of Blight
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE+TIMING_EQUIP)
+	c:RegisterEffect(e1)
+	--Special Summon 1 face-up spell/trap you control as a monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMING_END_PHASE+TIMING_EQUIP)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--search & destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.descon)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+end
+
+function s.spfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsOriginalType(TYPE_MONSTER)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and s.spfilter(chkc) end
+	if chk==0 then 
+		return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_SZONE,0,1,e:GetHandler():IsStatus(STATUS_CHAINING) and e:GetHandler() or nil) 
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_SZONE,0,1,1,e:IsHasType(EFFECT_TYPE_ACTIVATE) and e:GetHandler() or nil)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,LOCATION_SZONE)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and Duel.IsPlayerCanSpecialSummonMonster(tp,tc:GetCode(),nil,0x11,0,0,1,RACE_WYRM,ATTRIBUTE_DARK,POS_FACEUP_DEFENSE)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		tc:AddMonsterAttribute(TYPE_NORMAL,ATTRIBUTE_DARK,RACE_WYRM,9,0,0)
+		Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP_DEFENSE)
+		tc:AddMonsterAttributeComplete()
+		Duel.SpecialSummonComplete()
+	end
+end
+function s.thfilter(c)
+	return c:IsSetCard(0xf9) and c:IsAbleToHand() and not c:IsType(TYPE_MONSTER)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_ONFIELD) 
+		-- and c:IsPreviousPosition(POS_FACEUP)
+		-- and c:IsPreviousControler(tp) 
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then 
+		return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		if Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0xda),tp,LOCATION_ONFIELD,0,1,nil) 
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local tg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+			if #tg>0 then
+				Duel.SendtoHand(tg,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,tg)
+			end
+		end
+	end
+end
