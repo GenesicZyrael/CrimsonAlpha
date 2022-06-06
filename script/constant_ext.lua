025add74c9ccfa2b8ab7b33ddeadd3758de5ec00
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-06-07 00:22:17 +0800

    Add files via upload

---
+++ script/constant_ext.lua
@@ -0,0 +1,9 @@
+--Commonly used hardcoded effects
+EFFECT_ELEMENTAL_CONFLUX    = 55481029
+EFFECT_RITUAL_FROM_DECK		= 27200101
+EFFECT_EASY_FINAL_COUNTDOWN = 54334643
+
+--Flags for Card.RegisterEffect
+REGISTER_FLAG_SHADDOLL    	= 10 
+
+Duel.LoadScript("card_counter_constants_ext.lua")
---
+++ script/crimson_alpha.lua
@@ -0,0 +1,9 @@
+-- initialize script extensions
+Duel.LoadScript ("constant_ext.lua")
+Duel.LoadScript ("utility_ext.lua")
+Duel.LoadScript ("cards_specific_functions_ext.lua")
+Duel.LoadScript ("proc_ritual_ext.lua")
+Duel.LoadScript ("custom_set_codes.lua")
+
+-- update globals
+regeff_list[REGISTER_FLAG_SHADDOLL]=27200403
