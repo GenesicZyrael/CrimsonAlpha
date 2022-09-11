285ddcd4182db7b58826510d76867e349de0f8ed
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-09-11 19:34:43 +0800

    Update proc_ritual_ext.lua

--- script/proc_ritual_ext.lua
+++ script/proc_ritual_ext.lua
@@ -1,11 +1,38 @@
 Ritual.Target = aux.FunctionWithNamedArgs(
-function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselection,specificmatfilter,requirementfunc,sumpos)
+function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselection,specificmatfilter,requirementfunc,sumpos,extratg)
 	location = location or LOCATION_HAND
 	sumpos = sumpos or POS_FACEUP
 	return	function(e,tp,eg,ep,ev,re,r,rp,chk)
 				if chk==0 then
 					local mg=Duel.GetRitualMaterial(tp,not requirementfunc)
 					local mg2=extrafil and extrafil(e,tp,eg,ep,ev,re,r,rp,chk) or Group.CreateGroup()
+					--if an EFFECT_EXTRA_RITUAL_MATERIAL effect has a forcedselection of its own
+					--add that forcedselection to the one of the Ritual Spell, if any
+					local extra_eff_g=mg:Filter(Card.IsHasEffect,nil,EFFECT_EXTRA_RITUAL_MATERIAL)
+					if #extra_eff_g>0 then
+						local prev_repl_function=nil
+						for tmp_c in extra_eff_g:Iter() do
+							local effs={tmp_c:IsHasEffect(EFFECT_EXTRA_RITUAL_MATERIAL)}
+							for _,eff in ipairs(effs) do
+								local repl_function=eff:GetLabelObject()
+								if repl_function and prev_repl_function~=repl_function[1] then
+									prev_repl_function=repl_function[1]
+									if not forcedselection then
+										forcedselection=repl_function[1]
+									elseif forcedselection~=repl_function[1] then
+										forcedselection=(function()
+															local oldfunc=forcedselection
+															return function(e,tp,sg,sc)
+																local ret1,ret2=oldfunc(e,tp,sg,sc)
+																local repl1,repl2=repl_function[1](e,tp,sg,sc)
+																return ret1 and repl1,ret2 or repl2
+															end
+														end)()
+									end
+								end
+							end
+						end
+					end
 					Ritual.CheckMatFilter(matfilter,e,tp,mg,mg2)
 					--- START of INSERT: CrimsonAlpha
 					local fg=Group.CreateGroup()
@@ -14,16 +41,18 @@ function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselect
 					end
 					if #fg>0 then
 						location = location + LOCATION_DECK
-						return Duel.IsExistingMatchingCard(Ritual.Filter,tp,location,0,1,e:GetHandler(),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
-							or Duel.IsExistingMatchingCard(Ritual.Filter,tp,LOCATION_DECK,0,1,e:GetHandler(),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
+						-- return Duel.IsExistingMatchingCard(Ritual.Filter,tp,location,0,1,e:GetHandler(),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
+							-- or Duel.IsExistingMatchingCard(Ritual.Filter,tp,LOCATION_DECK,0,1,e:GetHandler(),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
 					else
-						return Duel.IsExistingMatchingCard(Ritual.Filter,tp,location,0,1,e:GetHandler(),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
+						-- return Duel.IsExistingMatchingCard(Ritual.Filter,tp,location,0,1,e:GetHandler(),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
 					end
-					--- END of INSERT: CrimsonAlpha						
+					--- END of INSERT: CrimsonAlpha	
+					return Duel.IsExistingMatchingCard(Ritual.Filter,tp,location,0,1,e:GetHandler(),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
 				end
+				if extratg then extratg(e,tp,eg,ep,ev,re,r,rp,chk) end
 				Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,location)
 			end
-end,"filter","lvtype","lv","extrafil","extraop","matfilter","stage2","location","forcedselection","specificmatfilter","requirementfunc","sumpos")
+end,"filter","lvtype","lv","extrafil","extraop","matfilter","stage2","location","forcedselection","specificmatfilter","requirementfunc","sumpos","extratg")
 
 Ritual.Operation = aux.FunctionWithNamedArgs(
 function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselection,customoperation,specificmatfilter,requirementfunc,sumpos)
@@ -32,6 +61,33 @@ function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselect
 	return	function(e,tp,eg,ep,ev,re,r,rp)
 				local mg=Duel.GetRitualMaterial(tp,not requirementfunc)
 				local mg2=extrafil and extrafil(e,tp,eg,ep,ev,re,r,rp) or Group.CreateGroup()
+				--if an EFFECT_EXTRA_RITUAL_MATERIAL effect has a forcedselection of its own
+				--add that forcedselection to the one of the Ritual Spell, if any
+				local extra_eff_g=mg:Filter(Card.IsHasEffect,nil,EFFECT_EXTRA_RITUAL_MATERIAL)
+				if #extra_eff_g>0 then
+					local prev_repl_function=nil
+					for tmp_c in extra_eff_g:Iter() do
+						local effs={tmp_c:IsHasEffect(EFFECT_EXTRA_RITUAL_MATERIAL)}
+						for _,eff in ipairs(effs) do
+							local repl_function=eff:GetLabelObject()
+							if repl_function and prev_repl_function~=repl_function[1] then
+								prev_repl_function=repl_function[1]
+								if not forcedselection then
+									forcedselection=repl_function[1]
+								elseif forcedselection~=repl_function[1] then
+									forcedselection=(function()
+														local oldfunc=forcedselection
+														return function(e,tp,sg,sc)
+															local ret1,ret2=oldfunc(e,tp,sg,sc)
+															local repl1,repl2=repl_function[1](e,tp,sg,sc)
+															return ret1 and repl1,ret2 or repl2
+														end
+													end)()
+								end
+							end
+						end
+					end
+				end
 				Ritual.CheckMatFilter(matfilter,e,tp,mg,mg2)
 				local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -54,7 +110,7 @@ function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselect
 									fc=fg:Select(tp,1,1,nil)
 								end
 								Duel.Hint(HINT_CARD,0,fc:GetCode())
-								fc:RegisterFlagEffect(27200101,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)							
+								fc:RegisterFlagEffect(27200101,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
 							end
 						end	
 					else
@@ -69,7 +125,7 @@ function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselect
 						fc:RegisterFlagEffect(27200101,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)					
 					end					
 				end
-				--- END of INSERT: CrimsonAlpha					
+				--- END of INSERT: CrimsonAlpha	
 				local tg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(Ritual.Filter),tp,location,0,1,1,e:GetHandler(),filter,_type,e,tp,mg,mg2,forcedselection,specificmatfilter,lv,requirementfunc,sumpos)
 				if #tg>0 then
 					local tc=tg:GetFirst()
@@ -97,7 +153,7 @@ function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselect
 						if tc.mat_filter then
 							mg:Match(tc.mat_filter,tc,tp)
 						end
-						if not mg:IsExists(Card.IsLocation,1,nil,LOCATION_OVERLAY) and ft>0 and not func then
+						if ft>0 and not func then
 							Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 							if _type==RITPROC_EQUAL then
 								mat=mg:SelectWithSumEqual(tp,requirementfunc or Card.GetRitualLevel,lv,1,#mg,tc)
@@ -106,6 +162,24 @@ function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselect
 							end
 						else
 							mat=aux.SelectUnselectGroup(mg,e,tp,1,lv,Ritual.Check(tc,lv,func,_type,requirementfunc),1,tp,HINTMSG_RELEASE,Ritual.Finishcon(tc,lv,requirementfunc,_type))
+						end
+					end
+					--check if a card from an "once per turn" EFFECT_EXTRA_RITUAL_MATERIAL effect was selected
+					local extra_eff_g=mat:Filter(Card.IsHasEffect,nil,EFFECT_EXTRA_RITUAL_MATERIAL)
+					for tmp_c in extra_eff_g:Iter() do
+						local effs={tmp_c:IsHasEffect(EFFECT_EXTRA_RITUAL_MATERIAL)}
+						for _,eff in ipairs(effs) do
+							--if eff is OPT and tmp_c is not returned
+							--by the Ritual Spell's exrafil
+							--then use the count limit and register
+							--the flag to turn the extra eff OFF
+							--requires the EFFECT_EXTRA_RITUAL_MATERIAL effect
+							--to check the flag in its condition
+							local _,max_count_limit=eff:GetCountLimit()
+							if max_count_limit>0 and not mg2:IsContains(tmp_c) then
+								eff:UseCountLimit(tp,1)
+								Duel.RegisterFlagEffect(tp,eff:GetHandler():GetCode(),RESET_PHASE+PHASE_END,0,1)
+							end
 						end
 					end
 					if not customoperation then
@@ -118,6 +192,7 @@ function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselect
 						Duel.BreakEffect()
 						Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,sumpos)
 						tc:CompleteProcedure()
+						if tc:IsFacedown() then Duel.ConfirmCards(1-tp,tc) end
 						if stage2 then
 							stage2(mat,e,tp,eg,ep,ev,re,r,rp,tc)
 						end
