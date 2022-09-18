12e2249b0014a65334dc83560341796b77592e95
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-09-18 22:30:26 +0800

    Revert changes from Nekroz Kaleidoscope
    Interaction with Nekroz of Mind Augus is buggy

--- script/custom/c51124303.lua
+++ script/custom/c51124303.lua
@@ -38,33 +38,11 @@ function s.rfilter(c,mc)
 	local lv=c:GetLevel()
 	return lv==(mlv&0xffff) or lv==(mlv>>16)
 end
---- START of INSERT: CrimsonAlpha
-function s.rfilter2(c,mc)
-	local mlv=mc:GetRitualLevel(c)
-	if mlv==mc:GetLevel() then return false end
-	local lv=c:GetLevel()
-	local mlv2=mlv&0xffff
-	if lv~=mlv2 then mlv2=lv else return false end
-	return lv==mlv2
-end
---- END of INSERT: CrimsonAlpha
 function s.filter(c,e,tp)
+	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,c,e,tp,c)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if c:IsLocation(LOCATION_MZONE) then ft=ft+1 end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	--- START of INSERT: CrimsonAlpha
-	local location=LOCATION_HAND
-	local fg=Group.CreateGroup()
-	for i,pe in ipairs({Duel.IsPlayerAffectedByEffect(tp,27200101)}) do
-		fg:AddCard(pe:GetHandler())
-	end
-	if #fg>0 then
-		location=LOCATION_HAND+LOCATION_DECK
-	else
-		location=LOCATION_HAND
-	end
-	--- END of INSERT: CrimsonAlpha
-	local sg=Duel.GetMatchingGroup(s.spfilter,tp,location,0,c,e,tp,c)
 	return sg:IsExists(s.rfilter,1,nil,c) or sg:CheckWithSumEqual(Card.GetLevel,c:GetLevel(),1,ft)
 end
 function s.mfilter(c)
@@ -82,21 +60,11 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 			local mg2=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_EXTRA,0,nil)
 			mg:Merge(mg2)
 		else
-			mg=mg:Filter(s.mzfilter,nil,tp)
+			mg:Match(s.mzfilter,nil,tp)
 		end
 		return mg:IsExists(s.filter,1,nil,e,tp)
 	end
-	--- START of INSERT: CrimsonAlpha
-	local fg=Group.CreateGroup()
-	for i,pe in ipairs({Duel.IsPlayerAffectedByEffect(tp,27200101)}) do
-		fg:AddCard(pe:GetHandler())
-	end
-	if #fg>0 then
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
-	else
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-	end
-	--- END of INSERT: CrimsonAlpha
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
@@ -106,7 +74,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local mg2=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_EXTRA,0,nil)
 		mg:Merge(mg2)
 	else
-		mg=mg:Filter(Card.IsLocation,nil,LOCATION_MZONE)
+		mg:Match(Card.IsLocation,nil,LOCATION_MZONE)
 	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local mat=mg:FilterSelect(tp,s.filter,1,1,nil,e,tp)
@@ -114,54 +82,12 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not mc then return end
 	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,mc,e,tp,mc)
 	if mc:IsLocation(LOCATION_MZONE) then ft=ft+1 end
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end	
-	--- START of INSERT: CrimsonAlpha
-	local sg1=Group.CreateGroup()	
-	local location=LOCATION_HAND
-	local fg=Group.CreateGroup()
-	for i,pe in ipairs({Duel.IsPlayerAffectedByEffect(tp,27200101)}) do
-		fg:AddCard(pe:GetHandler())
-	end
-	if #fg>0 then
-		if #sg>0 then
-			if sg:IsExists(s.rfilter,1,nil,mc) or sg:CheckWithSumEqual(Card.GetLevel,mc:GetLevel(),1,ft) then
-				if Duel.SelectYesNo(tp,aux.Stringid(27200101,0)) then
-					location=LOCATION_DECK
-				end
-			else
-				location=LOCATION_DECK
-			end
-		else
-			location=LOCATION_DECK
-		end
-	else
-		location=LOCATION_HAND
-	end
-	if location==LOCATION_DECK then
-		sg1=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,mc,e,tp,mc)
-		sg:Merge(sg1)
-		local fc=nil
-		if #fg==1 then
-			fc=fg:GetFirst()
-		else
-			fc=fg:Select(tp,1,1,nil)
-		end
-		Duel.Hint(HINT_CARD,0,fc:GetCode())
-		fc:RegisterFlagEffect(27200101,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)							
-	end
-	--- END of INSERT: CrimsonAlpha		
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	local b1=sg:IsExists(s.rfilter,1,nil,mc)
 	local b2=sg:CheckWithSumEqual(Card.GetLevel,mc:GetLevel(),1,ft)
 	if b1 and (not b2 or Duel.SelectYesNo(tp,aux.Stringid(id,0))) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=nil
-		--- START of INSERT: CrimsonAlpha
-		if #fg==1 then
-			tg=sg:FilterSelect(tp,s.rfilter2,1,1,nil,mc)
-		else
-			tg=sg:FilterSelect(tp,s.rfilter,1,1,nil,mc)
-		end
-		--- END of INSERT: CrimsonAlpha
+		local tg=sg:FilterSelect(tp,s.rfilter,1,1,nil,mc)
 		local tc=tg:GetFirst()
 		tc:SetMaterial(mat)
 		if not mc:IsLocation(LOCATION_EXTRA) then
@@ -174,20 +100,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		tc:CompleteProcedure()
 	else
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		--- START of INSERT: CrimsonAlpha
-		local lv=mc:GetLevel()
-		local tg,tg1
-		local tc,tc1
-		if #sg1>0 then
-			tg1=sg1:FilterSelect(tp,s.lvfilter,1,1,nil,lv,sg,sg1)
-			tc1=tg1:GetFirst()
-			tc1:SetMaterial(mat)
-			lv=lv-tc1:GetLevel()
-			sg:Sub(sg1)
-		end
-		--- END of INSERT: CrimsonAlpha
-		tg=sg:SelectWithSumEqual(tp,Card.GetLevel,lv,1,ft)
-		tc=tg:GetFirst()
+		local tg=sg:SelectWithSumEqual(tp,Card.GetLevel,mc:GetLevel(),1,ft)
+		local tc=tg:GetFirst()
 		for tc in aux.Next(tg) do
 			tc:SetMaterial(mat)
 		end
@@ -197,11 +111,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
 		end
 		Duel.BreakEffect()
-		--- START of INSERT: CrimsonAlpha
-		if tg1 then
-			tg:Merge(tg1)
-		end
-		--- END of INSERT: CrimsonAlpha
 		tc=tg:GetFirst()
 		for tc in aux.Next(tg) do
 			Duel.SpecialSummonStep(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
@@ -209,11 +118,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 		Duel.SpecialSummonComplete()
 	end
-end
-function s.lvfilter(c,lv,lsg,lsg1)
-	lsg:Sub(lsg1)
-	local lv1=lv-c:GetLevel()
-	return c:IsLevelBelow(lv) and lsg:IsExists(Card.IsLevel,1,nil,lv1)
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
