158cb5987e27380803027a115073fcd6a43b2937
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-10-22 00:20:51 +0800

    Update constant_ext.lua
    Updated constants

--- script/constant_ext.lua
+++ script/constant_ext.lua
@@ -1,10 +1,10 @@
 --Commonly used hardcoded effects
 EFFECT_ELEMENTAL_CONFLUX    = 55481029
-EFFECT_RITUAL_FROM_DECK		= 27200101
+CUSTOM_RITUAL_FROM_DECK		= 27200101
 EFFECT_DOUBLE_FC_TICK 		= 54334643
 EFFECT_REDUCE_FC_COUNT		= 99999999
 
 --Flags for Card.RegisterEffect
-REGISTER_FLAG_FLIP    	= 10
+REGISTER_FLAG_FLIP    		= 10
 
 Duel.LoadScript("card_counter_constants_ext.lua")
