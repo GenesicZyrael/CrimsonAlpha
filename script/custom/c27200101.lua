8cdf3685618f626a7c2a18f4001a22bdc2b3859f
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-10-22 19:32:51 +0800

    Update c27200101.lua

--- script/custom/c27200101.lua
+++ script/custom/c27200101.lua
@@ -3,7 +3,7 @@ Duel.LoadScript ("crimson_alpha.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2,2)
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2,2,s.lcheck)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -22,6 +22,10 @@ function s.initial_effect(c)
 	e2:SetCondition(s.spcon)
 	e2:SetOperation(s.sumop)
 	c:RegisterEffect(e2)
+end
+s.listed_series={0xb4}
+function s.lcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsSetCard,1,nil,0xb4,lc,sumtype,tp)
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
@@ -47,4 +51,23 @@ end
 function s.sumop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(tp,CUSTOM_RITUAL_LOCATION)~=0 then return end
 	Duel.RegisterFlagEffect(tp,CUSTOM_RITUAL_LOCATION,RESET_PHASE+PHASE_END,0,1,LOCATION_DECK)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,CUSTOM_RITUAL_LOCATION)~=0 then return end
+	Duel.RegisterFlagEffect(tp,CUSTOM_RITUAL_LOCATION,RESET_PHASE+PHASE_END,0,1,LOCATION_DECK)
+	local ge1=Effect.CreateEffect(e:GetHandler())
+	ge1:SetType(EFFECT_TYPE_FIELD)
+	ge1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	ge1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	ge1:SetTargetRange(1,0)
+	ge1:SetTarget(s.splimit)
+	ge1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(ge1,tp)
+	aux.RegisterClientHint(e:GetHandler(),EFFECT_FLAG_OATH,tp,1,0,aux.Stringid(id,2),nil)
+	--lizard check
+	aux.addTempLizardCheck(e:GetHandler(),tp)
+end
+	--Cannot special summon from extra deck
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsSetCard(0xb4) and c:IsLocation(LOCATION_EXTRA)
 end
