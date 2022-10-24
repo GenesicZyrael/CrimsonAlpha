8fa3eda0e5d2aacb16bda588fdb848519693e69f
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-10-25 00:44:23 +0800

    Update utility_ext.lua
    Added new functions for limiting cards on the field via ID

--- script/utility_ext.lua
+++ script/utility_ext.lua
@@ -33,3 +33,79 @@ function Card.RegisterEffect(c,e,forced,...)
 	return reg_e
 end
 
+local function AdjustOp(self,opp,limit,code,location)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		local c=e:GetHandler()
+		local phase=Duel.GetCurrentPhase()
+		local rm=Group.CreateGroup()
+		if (phase==PHASE_DAMAGE and not Duel.IsDamageCalculated()) or phase==PHASE_DAMAGE_CAL then return end
+		if self then
+			local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCode,code),tp,location,0,nil)
+			local rg=Group.CreateGroup()
+			local ct=#g-limit
+			if #g>limit then
+				Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(code,1))
+				rg=g:Select(1-tp,ct,ct,nil):GetFirst()
+				Duel.HintSelection(rg,true)
+			end
+			rm:Merge(rg)
+		end
+		if opp then
+			local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCode,code),tp,0,location,nil)
+			local rg=Group.CreateGroup()
+			local ct=#g-limit
+			if #g>limit then
+				Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(code,1))
+				rg=g:Select(1-tp,ct,ct,nil):GetFirst()
+				Duel.HintSelection(rg,true)
+			end
+			rm:Merge(rg)
+		end
+		if #rm>0 then			
+			Duel.SendtoGrave(rm,REASON_RULE)
+			Duel.Readjust()
+		end
+	end
+end
+local function FIDFilter(c,code)
+	return c:GetFieldID()~=code
+end
+local function SummonLimit(limit,code,location)
+	return function(e,c,sump,sumtype,sumpos,targetp)
+		if not c:IsCode(code) then return false end
+		local g=Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsCode,code),targetp or sump,location,0,1)
+		return g>limit-1
+	end
+end
+
+function Card.SetLimitIdOnField(c,self,opp,limit,code,location)
+	if location then
+		location=location
+	else
+		location=LOCATION_ONFIELD
+	end
+	--Adjust
+	local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EVENT_ADJUST)
+		e1:SetRange(LOCATION_MZONE+LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+		e1:SetOperation(AdjustOp(self,opp,limit,code,location))
+	c:RegisterEffect(e1)
+	--Cannot Normal/Flip/Special Summon
+	local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetRange(LOCATION_MZONE+LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+		e2:SetCode(EFFECT_CANNOT_SUMMON)
+		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e2:SetTargetRange(1,1)
+		e2:SetTarget(SummonLimit(limit,code,location))
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+		e3:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
+	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+		e4:SetCode(EFFECT_FORCE_SPSUMMON_POSITION)
+		e4:SetValue(POS_FACEDOWN)
+	c:RegisterEffect(e4)
+end
