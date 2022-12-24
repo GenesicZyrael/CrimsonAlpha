8a685e75b25264162a606a93507f012e2e499a62
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-12-24 12:20:08 +0800

    Update utility_ext.lua

--- script/utility_ext.lua
+++ script/utility_ext.lua
@@ -1,37 +1,67 @@
 -- regeff_list is a global table that we can add additional entries to, from outside of utility.lua
 regeff_list={[REGISTER_FLAG_DETACH_XMAT]=511002571,[REGISTER_FLAG_CARDIAN]=511001692,[REGISTER_FLAG_THUNDRA]=12081875,[REGISTER_FLAG_ALLURE_LVUP]=511310036}
 --for additional registers
-local regeff=Card.RegisterEffect
-function Card.RegisterEffect(c,e,forced,...)
-	if c:IsStatus(STATUS_INITIALIZING) and not e then
-		error("Parameter 2 expected to be Effect, got nil instead.",2)
-	end
-	local reg_e = regeff(c,e,forced)
-	if not reg_e then
-		return nil
-	end
-	local reg={...}
-	local resetflag,resetcount=e:GetReset()
-	for _,val in ipairs(reg) do
-		local setcode=regeff_list[val]
-		if setcode==nil then return end
-		local prop=EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE
-		if e:IsHasProperty(EFFECT_FLAG_UNCOPYABLE) then prop=prop|EFFECT_FLAG_UNCOPYABLE end
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetProperty(prop,EFFECT_FLAG2_MAJESTIC_MUST_COPY)
-		e2:SetCode(setcode)
-		e2:SetLabelObject(e)
-		e2:SetLabel(c:GetOriginalCode())
-		if resetflag and resetcount then
-			e2:SetReset(resetflag,resetcount)
-		elseif resetflag then
-			e2:SetReset(resetflag)
+
+Card.RegisterEffect=(function()
+	local oldf=Card.RegisterEffect
+	return function(c,e,forced,...)
+		local reg_e=oldf(c,e,forced)
+		if not reg_e or reg_e<=0 then return reg_e end
+		local resetflag,resetcount=e:GetReset()
+		for _,val in ipairs{...} do
+			local code=regeff_list[val]
+			if code then
+				local prop=EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE
+				if e:IsHasProperty(EFFECT_FLAG_UNCOPYABLE) then prop=prop|EFFECT_FLAG_UNCOPYABLE end
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetProperty(prop,EFFECT_FLAG2_MAJESTIC_MUST_COPY)
+				e2:SetCode(code)
+				e2:SetLabelObject(e)
+				e2:SetLabel(c:GetOriginalCode())
+				if resetflag and resetcount then
+					e2:SetReset(resetflag,resetcount)
+				elseif resetflag then
+					e2:SetReset(resetflag)
+				end
+				c:RegisterEffect(e2)
+			end
 		end
-		c:RegisterEffect(e2)
+		return reg_e
 	end
-	return reg_e
-end
+end)()
+
+-- local regeff=Card.RegisterEffect
+-- function Card.RegisterEffect(c,e,forced,...)
+	-- if c:IsStatus(STATUS_INITIALIZING) and not e then
+		-- error("Parameter 2 expected to be Effect, got nil instead.",2)
+	-- end
+	-- local reg_e = regeff(c,e,forced)
+	-- if not reg_e then
+		-- return nil
+	-- end
+	-- local reg={...}
+	-- local resetflag,resetcount=e:GetReset()
+	-- for _,val in ipairs(reg) do
+		-- local setcode=regeff_list[val]
+		-- if setcode==nil then return end
+		-- local prop=EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE
+		-- if e:IsHasProperty(EFFECT_FLAG_UNCOPYABLE) then prop=prop|EFFECT_FLAG_UNCOPYABLE end
+		-- local e2=Effect.CreateEffect(c)
+		-- e2:SetType(EFFECT_TYPE_SINGLE)
+		-- e2:SetProperty(prop,EFFECT_FLAG2_MAJESTIC_MUST_COPY)
+		-- e2:SetCode(setcode)
+		-- e2:SetLabelObject(e)
+		-- e2:SetLabel(c:GetOriginalCode())
+		-- if resetflag and resetcount then
+			-- e2:SetReset(resetflag,resetcount)
+		-- elseif resetflag then
+			-- e2:SetReset(resetflag)
+		-- end
+		-- c:RegisterEffect(e2)
+	-- end
+	-- return reg_e
+-- end
 
 local function CheckEffectUniqueCheck(c,tp,code)
 	if not (aux.FaceupFilter(Card.IsCode,code) and c:IsHasEffect(EFFECT_UNIQUE_CHECK)) then 
