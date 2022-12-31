f96177955f72342b67ac5d55a0b7fe716a79b0c0
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2023-01-01 02:45:04 +0800

    Update crimson_alpha.lua

--- script/crimson_alpha.lua
+++ script/crimson_alpha.lua
@@ -4,6 +4,7 @@ Duel.LoadScript ("utility_ext.lua")
 Duel.LoadScript ("cards_specific_functions_ext.lua")
 Duel.LoadScript ("proc_ritual_ext.lua")
 Duel.LoadScript ("proc_pendulum_ext.lua")
+Duel.LoadScript ("proc_link_ext.lua")
 Duel.LoadScript ("custom_set_codes.lua")
 
 -- update globals
