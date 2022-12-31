c4a472b6b38a4a5af06867376aedaf9d728b3087
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2022-12-31 13:38:52 +0800

    Update card_counter_constants_ext.lua

--- script/card_counter_constants_ext.lua
+++ script/card_counter_constants_ext.lua
@@ -11,7 +11,9 @@
 --Commonly used counters
 
 --Archetypes
-SET_INJECTION_FAIRY 		= 0x990
+SET_INJECTION_FAIRY 			= 0x990
 SET_SHINOBIRD 				= 0x991
 SET_GUARDRAGON 				= 0x992
 SET_METALMORPH 				= 0x993
+SET_W_NEBULA				= 0x994
+
