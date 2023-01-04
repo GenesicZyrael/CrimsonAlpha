d5c03c3424f14d6c2a36af1b386fca15a0d9cba0
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2023-01-04 10:38:07 +0800

    Add files via upload

--- script/custom/c10555403.lua
+++ script/custom/c10555403.lua
@@ -12,8 +12,10 @@ function s.initial_effect(c)
 	c:RegisterEffect(e0)
 	--extra summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.sumcon)
+	e1:SetTarget(s.sumtg)
 	e1:SetOperation(s.sumop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
@@ -28,12 +30,17 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 s.listed_names={74875003,CARD_RA,10000020,10000000}
+function s.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsPlayerCanAdditionalSummon(tp)
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanSummon(tp) end
+end
 function s.extg(e,c)
 	return c:IsCode(CARD_RA,10000020,10000000) 
 		or c:ListsCode(CARD_RA,10000020,10000000)
 end
 function s.sumop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(tp,id)~=0 then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -42,7 +49,16 @@ function s.sumop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTarget(s.extg)
 	Duel.RegisterEffect(e1,tp)
-	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	-- if Duel.GetFlagEffect(tp,id)~=0 then return end
+	-- local e1=Effect.CreateEffect(e:GetHandler())
+	-- e1:SetDescription(aux.Stringid(id,0))
+	-- e1:SetType(EFFECT_TYPE_FIELD)
+	-- e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	-- e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	-- e1:SetReset(RESET_PHASE+PHASE_END)
+	-- e1:SetTarget(s.extg)
+	-- Duel.RegisterEffect(e1,tp)
+	-- Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
 end
 function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	local rc=e:GetHandler():GetReasonCard()
