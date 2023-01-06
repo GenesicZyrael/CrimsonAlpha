c49d7e466e3893c8470f82cfc14dd1de447d6322
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2023-01-07 02:10:43 +0800

    Add files via upload

--- script/official/c88332693.lua
+++ script/official/c88332693.lua
@@ -1,19 +1,11 @@
 --バスター・モード・ゼロ
---Assault Mode Zero
---Scripted by AlphaKretin
 --Modified for CrimsonAlpha
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_END_PHASE)
+	local e1=AssaultMode.CreateProc(c,LOCATION_HAND)
 	e1:SetCountLimit(1,id)
-	e1:SetCost(s.spcost)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
 	c:RegisterEffect(e1)
 	--set
 	local e2=Effect.CreateEffect(c)
@@ -28,47 +20,6 @@ function s.initial_effect(c)
 end
 s.listed_series={0x104f}
 s.listed_names={CARD_ASSAULT_MODE}
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	e:SetLabel(1)
-	return true
-end
-function s.cfilter(c,e,tp,ft)
-	if c:IsType(TYPE_SYNCHRO) and (ft>0 or (c:GetSequence()<5 and c:IsControler(tp))) then
-		return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,c,e,tp)		
-	end
-end
-function s.spfilter(c,tc,e,tp)
-	local code=tc:GetCode()
-	local ocode=tc:GetOriginalCode()
-	if tc.assault_mode_all then
-		return c:IsSetCard(0x104f) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
-	else
-		return c:IsSetCard(0x104f) and c.assault_mode 
-			and (c.assault_mode==code or c.assault_mode==ocode)
-			and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
-	end
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then
-		if e:GetLabel()~=1 then return false end
-		e:SetLabel(0)
-		return ft>-1 and Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,e,tp,ft)
-	end
-	local rg=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,e,tp,ft)
-	Duel.SetTargetCard(rg:GetFirst())
-	Duel.Release(rg,REASON_COST)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local c=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):GetFirst()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,c,e,tp):GetFirst()
-	if tc and Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)>0 then
-		tc:CompleteProcedure()
-	end
-end
 function s.setfilter(c,cd)
 	return c:IsCode(CARD_ASSAULT_MODE) and c:IsSSetable()
 end
