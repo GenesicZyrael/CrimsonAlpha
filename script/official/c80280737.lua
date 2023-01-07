9c3aca7b0c8584345b2d8d25fd65eb93ec8a3c24
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2023-01-07 19:12:26 +0800

    Update c80280737.lua

--- script/official/c80280737.lua
+++ script/official/c80280737.lua
@@ -2,6 +2,7 @@
 --Modified for CrimsonAlpha
 local s,id=GetID()
 function s.initial_effect(c)
+	--Activate
 	local e1=AssaultMode.CreateProc(c,LOCATION_DECK)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
 	c:RegisterEffect(e1)
