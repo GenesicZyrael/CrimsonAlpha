b8b131e7aa4a5292965560e1aaafc8cad37d8be7
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2023-09-30 15:20:38 +0800

    scripts update
    * Ritual Beast Update

--- script/custom/c27200501.lua
+++ script/custom/c27200501.lua
@@ -1,45 +1,38 @@
 -- Ritual Beast Training
--- Add 1 "Ritual Beast" monster from your Deck to your hand. 
--- If this card is in your GY: You can banish this card and 1 other "Ritual Beast" card from your GY to activate one of these effects; 
---● Banish 1 "Ritual Beast" monster from your hand or GY.
---● Target 1 face-up monster you control; It is also treated as a "Spiritual Beast Tamer" monster while face-up on the field. 
---You can only use 1 "Ritual Beast Training" effect per turn, and only once that turn. 
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-		e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-		e1:SetType(EFFECT_TYPE_ACTIVATE)
-		e1:SetCode(EVENT_FREE_CHAIN)
-		e1:SetCountLimit(1,id)
-		e1:SetTarget(s.ActTarg)
-		e1:SetOperation(s.ActOp)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 	--Banish
 	local e2=Effect.CreateEffect(c)
-		e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-		e2:SetType(EFFECT_TYPE_IGNITION)
-		e2:SetRange(LOCATION_GRAVE)
-		e2:SetCountLimit(1,id)
-		e2:SetCost(s.BanCost)
-		e2:SetTarget(s.BanTarg)
-		e2:SetOperation(s.BanOp)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.rmcost)
+	e2:SetTarget(s.rmtarg)
+	e2:SetOperation(s.rmope)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0xb5,0x30b5}
+s.listed_series={SET_RITUAL_BEAST,SET_SPIRITUAL_BEAST_TAMER}
 -- {Activation Effect: Search Ritual Beast Monster}
 function s.ActFilter(c)
-	return c:IsSetCard(0xb5) 
+	return c:IsSetCard(SET_RITUAL_BEAST) 
 		and c:IsType(TYPE_MONSTER) 
 		and c:IsAbleToHand()
 end
-function s.ActTarg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		return Duel.IsExistingMatchingCard(s.ActFilter,tp,LOCATION_DECK,0,1,nil) 
-	end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.ActFilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.ActOp(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.ActFilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
@@ -48,52 +41,52 @@ function s.ActOp(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 -- {Graveyard Effect: Activate 1 of these effects}
-function s.CostFilter(c)
-	return c:IsSetCard(0xb5) 
+function s.cfilter(c)
+	return c:IsSetCard(SET_RITUAL_BEAST) 
 		and c:IsAbleToRemoveAsCost()
 end
-function s.BanCost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return aux.bfgcost(e,tp,eg,ep,ev,re,r,rp,0)
-		and Duel.IsExistingMatchingCard(s.CostFilter,tp,LOCATION_GRAVE,0,1,c) end
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,c) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.CostFilter,tp,LOCATION_GRAVE,0,1,1,c)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,c)
 	g:AddCard(c)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function s.TargFilter1(c)
-	return c:IsSetCard(0xb5) 
+function s.tgfilter1(c)
+	return c:IsSetCard(SET_RITUAL_BEAST) 
 		and c:IsSummonable(true,nil)
 end
-function s.TargFilter2(c)
-	return c:IsSetCard(0xb5) 
+function s.tgfilter2(c)
+	return c:IsSetCard(SET_RITUAL_BEAST) 
 		and c:IsType(TYPE_MONSTER)
 		and c:IsAbleToRemove()
 end
-function s.TargFilter3(c)
+function s.tgfilter3(c)
 	return c:IsFaceup() 
-		and not c:IsSetCard(0x30b5)
+		and not c:IsSetCard(SET_SPIRITUAL_BEAST_TAMER)
 end
-function s.BanTarg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rmtarg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then 
-		return Duel.IsExistingMatchingCard(s.TargFilter1,tp,LOCATION_HAND,0,1,nil)
-			or Duel.IsExistingMatchingCard(s.TargFilter2,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,nil)
-			or Duel.IsExistingTarget(s.TargFilter3,tp,LOCATION_MZONE,0,1,nil)
+		return Duel.IsExistingMatchingCard(s.tgfilter1,tp,LOCATION_HAND,0,1,nil)
+			or Duel.IsExistingMatchingCard(s.tgfilter2,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,nil)
+			or Duel.IsExistingTarget(s.tgfilter3,tp,LOCATION_MZONE,0,1,nil)
 	end
 	local off=1
 	local ops={}
 	local opval={}
-	-- if Duel.IsExistingMatchingCard(s.TargFilter1,tp,LOCATION_HAND,0,1,nil) then
-		-- ops[off]=aux.Stringid(id,0)
-		-- opval[off-1]=1
-		-- off=off+1
-	-- end
-	if Duel.IsExistingMatchingCard(s.TargFilter2,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,nil) then
+	if Duel.IsExistingMatchingCard(s.tgfilter1,tp,LOCATION_HAND,0,1,nil) then
+		ops[off]=aux.Stringid(id,0)
+		opval[off-1]=1
+		off=off+1
+	end
+	if Duel.IsExistingMatchingCard(s.tgfilter2,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,nil) then
 		ops[off]=aux.Stringid(id,1)
 		opval[off-1]=2
 		off=off+1
 	end
-	if Duel.IsExistingTarget(s.TargFilter3,tp,LOCATION_MZONE,0,1,nil) then
+	if Duel.IsExistingTarget(s.tgfilter3,tp,LOCATION_MZONE,0,1,nil) then
 		ops[off]=aux.Stringid(id,2)
 		opval[off-1]=3
 		off=off+1
@@ -109,18 +102,18 @@ function s.BanTarg(e,tp,eg,ep,ev,re,r,rp,chk)
 	elseif opval[op]==3 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 		e:SetLabel(3)
-		Duel.SelectTarget(tp,s.TargFilter3,tp,LOCATION_MZONE,0,1,1,nil)
+		Duel.SelectTarget(tp,s.tgfilter3,tp,LOCATION_MZONE,0,1,1,nil)
 	end
 end
-function s.BanOp(e,tp,eg,ep,ev,re,r,rp)
+function s.rmope(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1 then
-		local g=Duel.SelectMatchingCard(tp,s.TargFilter1,tp,LOCATION_HAND,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,s.tgfilter1,tp,LOCATION_HAND,0,1,1,nil)
 		local tc=g:GetFirst()
 		if tc then
 			Duel.Summon(tp,tc,true,nil)
 		end	
 	elseif e:GetLabel()==2 then
-		local g=Duel.SelectMatchingCard(tp,s.TargFilter2,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,s.tgfilter2,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil)
 		local tc=g:GetFirst()
 		if tc then
 			Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
@@ -129,12 +122,12 @@ function s.BanOp(e,tp,eg,ep,ev,re,r,rp)
 		local tc=Duel.GetFirstTarget()
 		if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 			local e1=Effect.CreateEffect(tc)
-				e1:SetDescription(aux.Stringid(id,3))
-				e1:SetType(EFFECT_TYPE_SINGLE)
-				e1:SetCode(EFFECT_ADD_SETCODE)
-				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-				e1:SetValue(0x30b5)
-				e1:SetReset(RESET_EVENT+0x1fe0000)
+			e1:SetDescription(aux.Stringid(id,3))
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_ADD_SETCODE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+			e1:SetValue(SET_SPIRITUAL_BEAST_TAMER)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
 			tc:RegisterEffect(e1)
 		end
 	end
--- script/custom/c27200502.lua
+++ script/custom/c27200502.lua
@@ -4,79 +4,79 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate(effect)
 	local e1=Effect.CreateEffect(c)
-		e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
-		e1:SetType(EFFECT_TYPE_ACTIVATE)
-		e1:SetCode(EVENT_CHAINING)
-		e1:SetCountLimit(1,id)
-		e1:SetCondition(s.ActCond)
-		e1:SetCost(s.ActCost)
-		e1:SetTarget(s.ActTarg)
-		e1:SetOperation(s.ActOp)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 	--Banish(effect)
 	local e2=Effect.CreateEffect(c)
-		e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-		e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-		e2:SetCode(EVENT_REMOVE)
-		e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-		e2:SetCountLimit(1,id)
-		e2:SetCondition(s.BanCon)
-		e2:SetCost(s.BanCost)
-		e2:SetTarget(s.BanTarg)
-		e2:SetOperation(s.BanOp)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.rmcon)
+	e2:SetCost(s.rmcost)
+	e2:SetTarget(s.rmtarg)
+	e2:SetOperation(s.rmope)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0xb5,0x30b5}
+s.listed_series={SET_RITUAL_BEAST}
 -- {Activate Effect: Negate Activation}
-function s.CostFilter(c)
-	return c:IsSetCard(0xb5) 
+function s.cfilter(c)
+	return c:IsSetCard(SET_RITUAL_BEAST) 
 		and c:IsAbleToRemoveAsCost()
 end
-function s.ActCond(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
 end
-function s.ActCost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.CostFilter,tp,LOCATION_GRAVE,0,1,nil) end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.CostFilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function s.ActTarg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return re:GetHandler():IsAbleToRemove() end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,re:GetHandler():GetLocation())
-	else
-		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,re:GetHandler():GetPreviousLocation())
+	if re:GetHandler():IsAbleToRemove() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
 	end
 end
-function s.ActOp(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Remove(eg,POS_FACEDOWN,REASON_EFFECT)
 	end
 end
 -- {Graveyard Effect: Shuffle 5, Draw 1}
-function s.BanCon(e,tp,eg,ep,ev,re,r,rp)
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_GRAVE)
 end
-function s.BanCostFilter(c)
-	return c:IsSetCard(0xb5) 
+function s.rmcfilter(c)
+	return c:IsSetCard(SET_RITUAL_BEAST) 
 		and c:IsAbleToDeckOrExtraAsCost()
 		and not c:IsCode(id)
 end
-function s.BanCost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.BanCostFilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,5,nil) end
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rmcfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,5,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,s.BanCostFilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,5,5,nil)
+	local g=Duel.SelectMatchingCard(tp,s.rmcfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,4,4,nil)
+	g:AddCard(c)
 	Duel.SendtoDeck(g,nil,5,REASON_COST)
 end
-function s.BanTarg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rmtarg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function s.BanOp(e,tp,eg,ep,ev,re,r,rp)
+function s.rmope(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
 end
