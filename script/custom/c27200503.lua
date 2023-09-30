0676de448464dafbc8858b069028164b23e00289
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2023-09-30 16:19:32 +0800

    Update c27200503.lua

--- script/custom/c27200503.lua
+++ script/custom/c27200503.lua
@@ -2,16 +2,26 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:SetSPSummonOnce(id)
-	--special summon
+	--Normal Summon itself from hand
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	--e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetTargetRange(POS_FACEUP_DEFENSE,0)
-	e1:SetCondition(s.spcon)
+	-- e1:SetCountLimit(1,{id,0})
+	e1:SetCost(s.sumcost)
+	e1:SetTarget(s.sumtarg)
+	e1:SetOperation(s.sumope)
 	c:RegisterEffect(e1)
+	-- --Special Summon
+	-- -- local e1=Effect.CreateEffect(c)
+	-- -- e1:SetType(EFFECT_TYPE_FIELD)
+	-- -- e1:SetCode(EFFECT_SPSUMMON_PROC)
+	-- -- e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
+	-- -- e1:SetRange(LOCATION_HAND)
+	-- -- e1:SetTargetRange(POS_FACEUP_DEFENSE,0)
+	-- -- e1:SetCondition(s.spcon)
+	-- -- c:RegisterEffect(e1)
 	--token
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
@@ -22,8 +32,26 @@ function s.initial_effect(c)
 	e2:SetTarget(s.tkntg)
 	e2:SetOperation(s.tknop)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	c:RegisterEffect(e3)
 end
-s.listed_series={0xb5,0x10b5}
+s.listed_series={SET_RITUAL_BEAST,SET_RITUAL_BEAST_TAMER}
+function s.sumcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsPublic() and c:GetFlagEffect(id)==0 end
+	c:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+end
+function s.sumtarg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsSummonable(true,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.sumope(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.Summon(tp,c,true,nil)
+end
 function s.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0,nil)==0
@@ -40,7 +68,7 @@ function s.tkntg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.tknop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>1
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,27200530,0x10b5,0x4011,200,1000,2,RACE_PSYCHIC,ATTRIBUTE_WIND) then
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,27200530,SET_RITUAL_BEAST_TAMER,0x4011,200,1000,2,RACE_PSYCHIC,ATTRIBUTE_WIND) then
 			local token=Duel.CreateToken(tp,27200530)
 			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 			local e1=Effect.CreateEffect(e:GetHandler())
@@ -70,5 +98,5 @@ function s.tknop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.matlimit(e,c)
 	if not c then return false end
-	return not c:IsSetCard(0xb5)
+	return not c:IsSetCard(SET_RITUAL_BEAST)
 end
