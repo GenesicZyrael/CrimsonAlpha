e6391ad8fa868923fb16224d493a3721913e9fff
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2023-11-22 02:30:33 +0800

    Add files via upload

--- script/custom/c27200010.lua
+++ script/custom/c27200010.lua
@@ -1,7 +1,6 @@
 -- Gem-Knight Zefranite
 local s,id=GetID()
-local fparams = {}
-local ftparam = {nil,nil,nil,nil,Fusion.ForcedHandler}
+local params={ }
 function s.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
@@ -14,34 +13,31 @@ function s.initial_effect(c)
 	e2:SetTargetRange(1,0)
 	e2:SetTarget(s.splimit)
 	c:RegisterEffect(e2)
-	--fusion summon
+	-- Allow cards in the Extra Deck and Pendulum Zones as fusion materials
 	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
 	e3:SetRange(LOCATION_PZONE)
+	e3:SetTargetRange(LOCATION_EXTRA+LOCATION_PZONE,0)
+	e3:SetTarget(s.ftg)
 	e3:SetCountLimit(1,{id,0})
-	e3:SetTarget(Fusion.SummonEffTG(table.unpack(fparams)))
-	e3:SetOperation(Fusion.SummonEffOP(table.unpack(fparams)))	
+	e3:SetValue(aux.TRUE)
 	c:RegisterEffect(e3)
-	--fusion substitute
+	--pendulum set
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_FUSION_SUBSTITUTE)
-	e4:SetCondition(s.subcon)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetTarget(s.pctg)
+	e4:SetOperation(s.pcop(Fusion.SummonEffTG(table.unpack(params)),Fusion.SummonEffOP(table.unpack(params))))
 	c:RegisterEffect(e4)
-	--fusion substitute
-	local e5=Effect.CreateEffect(c)
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e5:SetDescription(aux.Stringid(id,2))
-	e5:SetType(EFFECT_TYPE_IGNITION)
-	e5:SetRange(LOCATION_MZONE)
-	-- e5:SetCountLimit(1,{id,1})
-	e5:SetCountLimit(1)
-	e5:SetCost(s.cost)
-	-- e5:SetOperation(s.operation(Fusion.SummonEffTG(table.unpack(ftparam)),Fusion.SummonEffOP(table.unpack(ftparam))))
-	e5:SetOperation(s.operation)
-	c:RegisterEffect(e5)	
+	local e5=e4:Clone()
+	e5:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e5:SetCode(EVENT_TO_DECK)
+	e5:SetCondition(s.pccon)
+	c:RegisterEffect(e5)
 end
 s.listed_series={SET_GEM_KNIGHT,SET_ZEFRA}
 -- {Pendulum Summon Restriction: Zefra & Gem- Monsters}
@@ -49,55 +45,31 @@ function s.splimit(e,c,sump,sumtype,sumpos,targetp)
 	if c:IsSetCard(SET_GEM_KNIGHT) or c:IsSetCard(SET_ZEFRA) then return false end
 	return bit.band(sumtype,SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
 end
--- {Monster Effect: Fusion Substitute}
-function s.subcon(e)
-	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
+-- {Monster Effect: Place in Pendulum Zone, then Fusion Summon if possible}
+function s.ftg(e,c)
+	return c:IsFaceup() and c:IsCanBeFusionMaterial() 
+		and (c:IsSetCard(SET_GEM_KNIGHT) or c:IsSetCard(SET_ZEFRA)) 
 end
--- {Monster Effect: Fusion Tag + Polymerization}
-function s.filter(c,ec)
-	return c:IsType(TYPE_MONSTER) and not c:IsSummonCode(nil,SUMMON_TYPE_FUSION,PLAYER_NONE,ec:GetCode(nil,SUMMON_TYPE_FUSION))
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.pccon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil,c) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local cg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil,c)
-	Duel.ConfirmCards(1-tp,cg)
-	e:SetLabel(cg:GetFirst():GetCode())
+	return c:IsLocation(LOCATION_EXTRA) 
 end
--- function s.operation(fustg,fusop)
-	-- return function(e,tp,eg,ep,ev,re,r,rp)
-		-- local c=e:GetHandler()
-		-- if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-		-- local e1=Effect.CreateEffect(c)
-		-- e1:SetDescription(aux.Stringid(id,3))
-		-- e1:SetType(EFFECT_TYPE_SINGLE)
-		-- e1:SetCode(EFFECT_ADD_CODE)
-		-- e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-		-- e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		-- e1:SetValue(e:GetLabel())
-		-- e1:SetOperation(s.chngcon)
-		-- c:RegisterEffect(e1)
-		-- local fus=fustg(e,tp,eg,ep,ev,re,r,rp,0)
-		-- if fus and Duel.SelectYesNo(tp,aux.Stringid(id,1))  then
-			-- Duel.BreakEffect()
-			-- fusop(e,tp,eg,ep,ev,re,r,rp)
-		-- end
-	-- end
--- end 
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,3))
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_ADD_CODE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	e1:SetValue(e:GetLabel())
-	e1:SetOperation(s.chngcon)
-	c:RegisterEffect(e1)
-end 
-function s.chngcon(scard,sumtype,tp)
-	return (sumtype&MATERIAL_FUSION)~=0
+function s.pctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then 
+		return (Duel.CheckLocation(tp,LOCATION_PZONE,0) 
+			or Duel.CheckLocation(tp,LOCATION_PZONE,1)) 
+	end	
+end
+function s.pcop(fustg,fusop)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		local c=e:GetHandler()
+		if not e:GetHandler():IsRelateToEffect(e) then return end
+		if not Duel.CheckLocation(tp,LOCATION_PZONE,0) and not Duel.CheckLocation(tp,LOCATION_PZONE,1) then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+		if fustg(e,tp,eg,ep,ev,re,r,rp,0) and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			Duel.BreakEffect()
+			fusop(e,tp,eg,ep,ev,re,r,rp)
+		end
+	end
 end
