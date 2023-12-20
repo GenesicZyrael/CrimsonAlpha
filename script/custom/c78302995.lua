b6d0715442c3aacdfa7de2d9ef4335f14d87c53d
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2023-12-20 08:58:32 +0800

    Add files via upload

--- script/custom/c78302995.lua
+++ script/custom/c78302995.lua
@@ -32,7 +32,7 @@ function s.fextra(e,tp,mg)
 	return nil
 end
 function s.exfilter(c)
-	return c:IsMonster() and c:IsCode(CARD_BUSTER_BLADER) and c:IsAbleToGrave()
+	return c:IsMonster() and c:IsAbleToGrave()
 end
 function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
