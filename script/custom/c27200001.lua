b4b945b9a8cb24f835c65f1df41eb66c824c345b
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-01-17 18:34:12 +0800

    Zefra Rework

---
+++ script/custom/c27200001.lua
@@ -0,0 +1,192 @@
+--Zefratorah Metaltron
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableUnsummonable()
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--Destroy 1 card to Special Summon from Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_HAND+LOCATION_EXTRA)
+	e2:SetCondition(s.spcon)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--Apply the effect of 1 "Zefra" monster
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+	--to deck
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,3))
+	e4:SetCategory(CATEGORY_TODECK)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetHintTiming(0,TIMING_END_PHASE)
+	e4:SetCountLimit(1,{id,2})
+	-- e4:SetCost(s.tdcost)
+	e4:SetTarget(s.tdtg)
+	e4:SetOperation(s.tdop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={SET_ZEFRA}
+s.listed_names={29432356}
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_ZEFRA)
+		and c:IsLevelBelow(6)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_PENDULUM,tp,false,false)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local loc=LOCATION_HAND+LOCATION_ONFIELD
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then loc=LOCATION_ONFIELD end
+	if chk==0 then 
+		return Duel.IsExistingMatchingCard(nil,tp,loc,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	local g=Duel.GetMatchingGroup(nil,tp,loc,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local loc=LOCATION_HAND+LOCATION_ONFIELD
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then loc=LOCATION_ONFIELD end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,loc,0,1,1,nil)
+	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+		if (tc:IsCode(29432356) and Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)>0)
+		or (tc and Duel.SpecialSummon(tc,SUMMON_TYPE_PENDULUM,tp,tp,false,false,POS_FACEUP)>0)	then
+			if c:IsLocation(LOCATION_PZONE) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+				scale=7
+			else
+				scale=1
+			end
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CHANGE_LSCALE)
+			e1:SetValue(scale)
+			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+			c:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_CHANGE_RSCALE)
+			e2:SetValue(scale)
+			c:RegisterEffect(e2)
+		end
+	end
+end
+
+function s.rescon(sg,e,tp,mg)
+	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:IsExists(Card.IsSetCard,1,nil,SET_ZEFRA)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetReleaseGroup(tp)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-3 and #rg>2 
+		and rg:IsExists(Card.IsSetCard,1,nil,SET_ZEFRA) 
+		and aux.SelectUnselectGroup(rg,e,tp,3,3,s.rescon,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetReleaseGroup(tp)
+	local sg=aux.SelectUnselectGroup(g,e,tp,3,3,s.rescon,1,tp,HINTMSG_RELEASE)
+	Duel.Release(sg,REASON_COST)
+	e:GetHandler():SetMaterial(sg)
+end
+
+function s.filter(c,tp)
+	if not (c:IsSetCard(SET_ZEFRA) and c:IsType(TYPE_PENDULUM)
+		and c:IsHasEffect(id)) then
+		return false
+	end
+	local eff=c:GetCardEffect(id)
+	local te=eff:GetLabelObject()
+	local con=te:GetCondition()
+	local tg=te:GetTarget()
+	if (not tg or tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,eff,REASON_EFFECT,PLAYER_NONE,0)) then
+		return true
+	end
+	return false
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE|LOCATION_DECK,0,1,nil,tp) end
+	local sc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE|LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+	Duel.SendtoExtraP(sc,tp,REASON_EFFECT)
+	sc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_CHAIN,0,1)
+	e:SetLabelObject(sc:GetCardEffect(id):GetLabelObject())
+	local te=e:GetLabelObject()
+	local tg=te and te:GetTarget() or nil
+	if chkc then return tg and tg(e,tp,eg,ep,ev,re,r,rp,0,chkc) end
+	e:SetLabel(te:GetLabel())
+	e:SetLabelObject(te:GetLabelObject())
+	e:SetProperty(te:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and EFFECT_FLAG_CARD_TARGET or 0)
+	if tg then
+		tg(e,tp,eg,ep,ev,re,r,rp,1)
+	end
+	e:SetLabelObject(te)
+	Duel.ClearOperationInfo(0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local te=e:GetLabelObject()
+	if not te then return end
+	local sc=te:GetHandler()
+	if sc:GetFlagEffect(id)==0 then
+		e:SetLabel(0)
+		e:SetLabelObject(nil)
+		return
+	end
+	e:SetLabel(te:GetLabel())
+	e:SetLabelObject(te:GetLabelObject())
+	local op=te:GetOperation()
+	if op then
+		op(e,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE)
+	end
+	e:SetLabel(0)
+	e:SetLabelObject(nil)
+end
+
+function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if not Duel.CheckLocation(1-tp,LOCATION_PZONE,0) and not Duel.CheckLocation(1-tp,LOCATION_PZONE,1) then return end
+end
+function s.tdfilter(c)
+	return c:IsAbleToDeck()
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) and chkc:IsAbleToDeck() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if tc and tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)>0 then
+		Duel.BreakEffect()
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true) 
+	end
+end
