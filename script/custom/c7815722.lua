c320c283ac876f3eece02126eeab5779946f1980
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-03-24 02:06:32 +0800

    Add files via upload

--- script/custom/c7815722.lua
+++ script/custom/c7815722.lua
@@ -7,20 +7,22 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	local e2=Ritual.AddProcGreater({handler=c,filter=s.ritualfil,desc=aux.Stringid(id,1),extrafil=s.extrafil})
 	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.condition)
 	e2:SetCost(s.cost)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0xda}
 s.fit_monster={98287529}
 s.listed_names={98287529}
-function s.cfilter(c)
-	return c:IsCode(98287529) and not c:IsPublic() 
+
+function s.costfilter(c)
+	return c:IsCode(98287529) and not c:IsPublic()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	if Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) then
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	if Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-		local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
 		Duel.ConfirmCards(1-tp,g)
 		Duel.ShuffleHand(tp)
 		e:SetLabel(1)
@@ -29,14 +31,15 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 function s.ritualfil(c)
-	return c:IsSetCard(0xda) --and c:IsRitualMonster()
+	return c:IsSetCard(0xda)
 end
 function s.mfilter(c,e)
-	return c:IsFaceup() and c:GetLevel()>0 and not c:IsImmuneToEffect(e) and c:IsReleasable()
+	return c:IsFaceup() and c:GetLevel()>0 and not c:IsImmuneToEffect(e) 
+		and c:IsReleasable()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.mfilter,tp,0,LOCATION_MZONE,1,nil,e) 
 end
 function s.extrafil(e,tp,eg,ep,ev,re,r,rp)
-	-- if e:GetLabel()==1 then
-		return Duel.GetMatchingGroup(s.mfilter,tp,0,LOCATION_MZONE,nil,e)
-	-- end
-	-- return nil
+	return Duel.GetMatchingGroup(s.mfilter,tp,0,LOCATION_MZONE,nil,e)
 end
