97ec01e1e0c0c9349c2c910e4b87c444fb2af4ff
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-03-28 18:48:36 +0800

    Amorphage Update

--- script/custom/c59207982.lua
+++ script/custom/c59207982.lua
@@ -2,77 +2,46 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	Pendulum.AddProcedure(c)
-	--Revive limit
-	c:EnableUnsummonable()
+	--maintenance cost
+	aux.AmorphageMCost(c)
+	--Special Summon condition
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_REVIVE_LIMIT)
-	e0:SetCondition(s.rvlimit)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(s.splimit)
 	c:RegisterEffect(e0)
-	--Special Summon condition
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(s.splimit)
-	c:RegisterEffect(e1)
-	--maintenance cost
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetCountLimit(1)
-	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e2:SetCondition(s.descon)
-	e2:SetOperation(s.desop)
-	c:RegisterEffect(e2)
 	--special summon limit
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetRange(LOCATION_PZONE)
-	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetTargetRange(1,1)
-	e3:SetCondition(s.sumcon)
-	e3:SetTarget(s.sumlimit)
-	c:RegisterEffect(e3)
-	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e1:SetCode(EVENT_DESTROYED)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon)
-	e1:SetOperation(s.regop)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,1)
+	e1:SetCondition(s.sumcon)
+	e1:SetTarget(s.sumlimit)
 	c:RegisterEffect(e1)
-	--Lizard check
-	aux.addContinuousLizardCheck(c,LOCATION_MZONE,s.lizfilter,0xff,0xff)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_RELEASE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_DESTROYED)
+	c:RegisterEffect(e3)
 end
 s.listed_names={7815722}
 s.listed_series={SET_DRACOVERLORD,SET_AMORPHAGE}
-function s.rvlimit(e)
-	return not e:GetHandler():IsLocation(LOCATION_HAND)
-end
 function s.splimit(e,se,sp,st)
-	return (st&SUMMON_TYPE_RITUAL)==SUMMON_TYPE_RITUAL or ((st&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
-		and e:GetHandler():IsLocation(LOCATION_HAND))
-end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	Duel.HintSelection(Group.FromCards(c),true)
-	if Duel.CheckReleaseGroup(tp,Card.IsReleasableByEffect,1,c) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		local g=Duel.SelectReleaseGroup(tp,Card.IsReleasableByEffect,1,1,c)
-		Duel.Release(g,REASON_COST)
-	else
-		Duel.Destroy(c,REASON_COST) 
-	end
+	return (st&SUMMON_TYPE_RITUAL)==SUMMON_TYPE_RITUAL 
+		or (st&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
 end
 function s.sumcon(e)
 	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_AMORPHAGE),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
@@ -80,63 +49,41 @@ end
 function s.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return c:IsLocation(LOCATION_DECK) and not c:IsType(TYPE_PENDULUM)
 end
-function s.lizfilter(e,c)
-	return not c:IsOriginalSetCard(SET_AMORPHAGE)
-end
-
-function s.regtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_DECK,0,1,nil,TYPE_PENDULUM) 
-	end
-end
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetCountLimit(1)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-end
 function s.cfilter(c,tp)
-	return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousControler(tp) 
-		and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEUP)
-		and (c:IsPreviousSetCard(SET_DRACOVERLORD) or c:IsPreviousSetCard(SET_AMORPHAGE))
+	return c:IsPreviousLocation(LOCATION_ALL) 
+		and c:IsPreviousControler(tp)
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
-function s.spfilter(c,e,tp,b1,setcode)
-	return c:IsLevelBelow(4) and c:IsSetCard(setcode) and c:IsType(TYPE_PENDULUM) and not c:IsForbidden()
+function s.spfilter(c,e,tp,b1)
+	return c:IsLevelBelow(4) 
+		and (c:IsSetCard(SET_AMORPHAGE) or c:IsSetCard(SET_DRACOVERLORD))
+		and c:IsType(TYPE_PENDULUM) and not c:IsForbidden()
 		and (b1 or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)
 	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	if chk==0 then return (b1 or b2)
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,b1,SET_DRACOVERLORD)
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,b1,SET_AMORPHAGE) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,b1) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_DECK)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local b1=Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)
 	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	if not b1 and not b2 then return end
-	local g1=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp,b1,SET_DRACOVERLORD)
-	local g2=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp,b1,SET_AMORPHAGE)
-	if #g1==0 or #g2==0 then return end
-	g1:Merge(g2)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local sg1=g1:Select(tp,1,1,nil)
-	Duel.ConfirmCards(1-tp,sg1)
+	local tc=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp,b1):Select(tp,1,1,nil):GetFirst()
+	Duel.ConfirmCards(1-tp,tc)
 	Duel.ShuffleDeck(tp)
-	local cg=sg1:Select(tp,1,1,nil)
-	local tc=cg:GetFirst()
-	Duel.Hint(HINT_CARD,0,tc:GetCode())
-	if b1 and (not b2 or not tc:IsCanBeSpecialSummoned(e,0,tp,false,false) or Duel.SelectYesNo(tp,aux.Stringid(id,1))) then
-		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
-	else
+	if not tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then b2=nil end
+	local op=b1 and Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,2)},
+		{b2,aux.Stringid(id,3)})
+	if op==1 then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	elseif op==2 then
+		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end
