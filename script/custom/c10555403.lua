c65599f7c17e2c3380a207f77347b9b464ecc410
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-05-20 21:14:01 +0800

    Add files via upload

--- script/custom/c10555403.lua
+++ script/custom/c10555403.lua
@@ -5,46 +5,51 @@ function s.initial_effect(c)
 	--Change its name to "Ra's Disciple"
 	local e0=aux.ChangeCode(c,74875003,LOCATION_MZONE+LOCATION_HAND+LOCATION_DECK)
 	c:RegisterEffect(e0)
-	--extra summon
+	--tohand
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCondition(s.sumcon)
-	e1:SetTarget(s.sumtg)
-	e1:SetOperation(s.sumop)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	--Disciple's Sacrifice
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_BE_PRE_MATERIAL)
-	e3:SetCondition(s.regcon)
-	e3:SetOperation(s.regop)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
+	--Disciple's Sacrifice
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_BE_PRE_MATERIAL)
+	e4:SetCondition(s.regcon)
+	e4:SetOperation(s.regop)
+	c:RegisterEffect(e4)
 end
 s.listed_names={74875003,CARD_RA,10000020,10000000}
-function s.sumcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsPlayerCanAdditionalSummon(tp)
+function s.thfilter(c)
+	return (c:IsCode(CARD_RA,10000020,10000000) or c:ListsCode(CARD_RA,10000020,10000000))
+		and c:IsAbleToHand() and not c:IsOriginalCode(id)
 end
-function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanSummon(tp) end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.extg(e,c)
-	return c:IsCode(CARD_RA,10000020,10000000) 
-		or c:ListsCode(CARD_RA,10000020,10000000)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
 end
-function s.sumop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
-	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetTarget(s.extg)
-	Duel.RegisterEffect(e1,tp)
-end
+
 function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	local rc=e:GetHandler():GetReasonCard()
 	return r==REASON_SUMMON and rc:IsFaceup() and rc:IsAttribute(ATTRIBUTE_DIVINE)
@@ -52,33 +57,28 @@ end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=c:GetReasonCard()
-	--immune
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_IMMUNE_EFFECT)
-	e1:SetCondition(s.immcon)
-	e1:SetValue(s.efilter)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	rc:RegisterEffect(e1)	
-end
-function s.immcon(e)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_NORMAL)
-end
-function s.efilter(e,te)
-	if te:IsActiveType(TYPE_MONSTER) and te:IsActivated() then
-		local lv=e:GetHandler():GetLevel()
-		local ec=te:GetOwner()
-		if ec:IsType(TYPE_LINK) then
-			return ec:GetLink()<=lv
-		elseif ec:IsType(TYPE_XYZ) then
-			return ec:GetOriginalRank()<=lv
-		else
-			return ec:GetOriginalLevel()<=lv
+	if rc:IsSummonType(SUMMON_TYPE_TRIBUTE) and rc:IsAttribute(ATTRIBUTE_DIVINE) then
+		local g=rc:GetMaterial()
+		local atk=0
+		local def=0
+		for gc in aux.Next(g) do
+			atk=atk+gc:GetBaseAttack()
+			def=def+gc:GetBaseDefense()
 		end
-	else
-		return false
+		if atk>4000 then atk=4000 end
+		if def>4000 then def=4000 end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetDescription(aux.Stringid(id,1))
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_SET_BASE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+		rc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_BASE_DEFENSE)
+		e2:SetValue(def)
+		rc:RegisterEffect(e2)
 	end
 end
+
