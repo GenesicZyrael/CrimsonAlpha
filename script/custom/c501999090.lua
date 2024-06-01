12392c138c685b919ff93c8e57e4c7a26591fd27
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-06-01 21:56:50 +0800

    Add files via upload

---
+++ script/custom/c501999090.lua
@@ -0,0 +1,94 @@
+--War Rock Aroy
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,s.mfilter,1,1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--force attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCost(s.cost)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(s.eftg)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_WAR_ROCK}
+function s.mfilter(c,lc,sumtype,tp)
+	return c:IsSetCard(SET_WAR_ROCK,lc,sumtype,tp)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function s.thfilter(c)
+	return c:IsSetCard(SET_WAR_ROCK) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.eftg(e,c)
+	return c:IsType(TYPE_EFFECT) and c:IsSetCard(SET_WAR_ROCK) 
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return not c:IsStatus(STATUS_BATTLE_DESTROYED) 
+		and not c:IsStatus(STATUS_CHAINING) 
+		and c:GetFlagEffect(id)==0
+end
+function s.filter(c)
+	return c:IsMonster()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then 
+		return chkc:IsControler(1-tp) 
+			and chkc:IsLocation(LOCATION_MZONE) 
+			and s.filter(chkc) 
+	end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		-- Duel.ForceAttack(c,tc)
+		Duel.CalculateDamage(c,tc)
+		c:RegisterFlagEffect(id,RESET_PHASE+PHASE_END,0,1)
+	end
+end
