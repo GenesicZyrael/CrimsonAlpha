d23a04b4af43092c92faa5cf007310ff0db3b504
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-06-26 10:02:56 +0800

    Script added
    Charge of Gusto Rework

--- script/custom/c27100504.lua
+++ script/custom/c27100504.lua
@@ -6,18 +6,15 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--synchro summon
+	--destroy 1 card you control
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCondition(s.sccon)
-	e2:SetCost(s.sccost)
-	e2:SetTarget(s.sctg)
-	e2:SetOperation(s.scop)
+	e2:SetCountLimit(1,{id,0})
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 	--Summon from the Deck
 	local e3=Effect.CreateEffect(c)
@@ -27,42 +24,50 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_DESTROYED)
 	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,id)
+	e3:SetCountLimit(1,{id,1})
 	e3:SetCondition(s.spcon)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
 end
-s.listed_series={0x10}
-function s.scfilter(c,tp)
-	return c:IsFaceup() and c:IsSetCard(0x10) and c:IsControler(tp)
+s.listed_series={SET_GUSTO}
+function s.desfilter(c,e,tp)
+	return c:IsSetCard(SET_GUSTO)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.sccon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.scfilter,1,nil,tp)
-end
-function s.sccost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:GetFlagEffect(id)==0 end
-	c:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+	local loc=LOCATION_ONFIELD
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then loc=LOCATION_MZONE end
+	if chk==0 then 
+		return Duel.IsExistingMatchingCard(nil,tp,loc,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	local g=Duel.GetMatchingGroup(nil,tp,loc,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0x10)
-end
-function s.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
-		return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,nil,mg)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-function s.scop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
-	local g=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,nil,mg)
-	if #g>0 then
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local loc=LOCATION_ONFIELD
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then loc=LOCATION_MZONE end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,loc,0,1,1,nil)
+	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:Select(tp,1,1,nil)
-		Duel.SynchroSummon(tp,sg:GetFirst(),nil,mg)
+		local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+		local r1=Effect.CreateEffect(c)
+		r1:SetDescription(aux.Stringid(id,2))
+		r1:SetType(EFFECT_TYPE_FIELD)
+		r1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		r1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		r1:SetTargetRange(1,0)
+		r1:SetTarget(s.splimit)
+		r1:SetReset(RESET_PHASE|PHASE_END)
+		Duel.RegisterEffect(r1,tp)
 	end
 end
 function s.cfilter(c,tp)
@@ -73,7 +78,8 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x10) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(SET_GUSTO) 
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -98,7 +104,16 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		r1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(r1,tp)
 	end
+	local r2=Effect.CreateEffect(c)
+	r2:SetDescription(aux.Stringid(id,2))
+	r2:SetType(EFFECT_TYPE_FIELD)
+	r2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	r2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	r2:SetTargetRange(1,0)
+	r2:SetTarget(s.splimit)
+	r2:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(r2,tp)
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp)
-	return not c:IsSetCard(0x10)
+	return not c:IsSetCard(SET_GUSTO)
 end
