127624544b7aa14c398957550b9f4b32e2e4bc81
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-07-15 15:02:20 +0800

    Bug Fix
    - Infestation Assault - Forgot to delete SetOperation function on card activation

--- script/custom/c27100205.lua
+++ script/custom/c27100205.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1,{id,0})
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	-- e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 	--cannot disable summon
