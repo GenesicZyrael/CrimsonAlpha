18a728fe23b47aa575fdf186a24689ceb06705e1
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-08-08 23:12:01 +0800

    Add files via upload

--- script/official/c74875003.lua
+++ script/official/c74875003.lua
@@ -2,6 +2,7 @@
 --Ra's Disciple
 local s,id=GetID()
 function s.initial_effect(c)
+	c:SetLimitOnField(3,id)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -42,18 +43,9 @@ function s.initial_effect(c)
 end
 s.listed_names={id,CARD_RA,10000020,10000000}
 function s.filter(c,e,tp)
-	--- Custom ---
-	local ct=Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsCode,id),tp,LOCATION_MZONE,0,nil)
-	local cond=c:IsCode(id)
-	if ct>2 then cond=c:IsOriginalCode(id) end
-	--------------
-	return cond and c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
+	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	--- Custom ---
-	local ct=aux.GetLimitForCardCount(tp,id,nil,LOCATION_MZONE)
-	if ct<1 then return end
-	--------------
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND)
@@ -62,17 +54,11 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<=0 then return end
 	if ft>2 then ft=2 end
-	--- Custom ---
-	local ct=aux.GetLimitForCardCount(tp,id,nil,LOCATION_MZONE)
-	if ct>2 then ct=2 end
-	if ft>ct then ft=ct end
-	--------------
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,ft,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		Duel.SpecialSummonComplete()
 	end
 end
 function s.sumval(e,c)
