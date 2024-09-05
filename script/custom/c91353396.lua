13b4d75a57c49d4b09bce74e6dbf626da0b0b542
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2024-09-06 00:41:54 +0800

    Add files via upload

--- script/custom/c91353396.lua
+++ script/custom/c91353396.lua
@@ -3,49 +3,53 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,false,false,aux.FilterBoolFunctionEx(Card.IsSetCard,0xed),1,aux.FilterBoolFunctionEx(Card.IsType,TYPE_FLIP),2)
+	Fusion.AddProcMixN(c,false,false,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_SUBTERROR_BEHEMOTH),1,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_SUBTERROR),1)
 	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
-	--spsummon
+	--Special Summon from Deck
 	local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(id,1))
-		e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-		e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-		e1:SetCountLimit(1,id)
-		e1:SetCode(EVENT_FLIP)
-		e1:SetTarget(s.sptg1)
-		e1:SetOperation(s.spop1)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetTarget(s.sptg1)
+	e1:SetOperation(s.spop1)
 	c:RegisterEffect(e1)
-	--special summon
-	local e2=Effect.CreateEffect(c)
-		e2:SetDescription(aux.Stringid(id,1))
-		e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-		e2:SetRange(LOCATION_GRAVE)
-		e2:SetCode(EVENT_CHANGE_POS)
-		e2:SetCondition(s.spcon2)
-		e2:SetTarget(s.sptg2)
-		e2:SetOperation(s.spop2)
-	c:RegisterEffect(e2)	
-	--disable
+	local e2=e1:Clone()
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_FLIP)
+	c:RegisterEffect(e2)
+	--Special Summon itself
 	local e3=Effect.CreateEffect(c)
-		e3:SetDescription(aux.Stringid(id,1))
-		e3:SetCategory(CATEGORY_DISABLE)
-		e3:SetType(EFFECT_TYPE_QUICK_O)
-		e3:SetCode(EVENT_FREE_CHAIN)
-		e3:SetRange(LOCATION_MZONE)
-		e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-		e3:SetCountLimit(1)
-		e3:SetTarget(s.distg)
-		e3:SetOperation(s.disop)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_CHANGE_POS)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.spcon2)
+	e3:SetTarget(s.sptg2)
+	e3:SetOperation(s.spop2)
 	c:RegisterEffect(e3)	
+	--negate
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_DISABLE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetTarget(s.distg)
+	e4:SetOperation(s.disop)
+	c:RegisterEffect(e4)	
 end
-s.listed_series={0xed}
+s.listed_series={SET_SUBTERROR_BEHEMOTH,SET_SUBTERROR}
 function s.contactfil(tp)
 	return Duel.GetReleaseGroup(tp)
 end
 function s.contactop(g,tp)
-	-- Duel.ConfirmCards(1-tp,g)
 	Duel.Release(g,REASON_COST+REASON_MATERIAL)
 end
 function s.splimit(e,se,sp,st)
@@ -53,7 +57,7 @@ function s.splimit(e,se,sp,st)
 end
 
 function s.spfilter1(c,e,tp)
-	return c:IsSetCard(0xed) and c:IsCanBeSpecialSummoned(e,156,tp,false,false)
+	return c:IsSetCard(SET_SUBTERROR) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -65,15 +69,15 @@ function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,156,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 
 function s.cfilter(c,tp)
-	return c:IsPreviousPosition(POS_FACEUP) and c:IsFacedown() and c:IsControler(tp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsFacedown()
 end
 function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+	return eg:IsExists(s.cfilter,1,nil)
 		and not Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -87,15 +91,11 @@ function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 end
-
-function s.disfilter(c)
-	return aux.disfilter1(c)
-end
 function s.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.disfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.disfilter,tp,0,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and s.disfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.disfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
