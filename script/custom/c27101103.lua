1d3dda05d30cdf81a6cb2e9c8b09721eb6739ed3
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2025-01-01 22:49:34 +0800

    Update c27101103.lua
    bug fix

--- script/custom/c27101103.lua
+++ script/custom/c27101103.lua
@@ -41,7 +41,7 @@ function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_SZONE,0,1,nil)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 and Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_SZONE,0,1,1,nil)~=0 then return true end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
